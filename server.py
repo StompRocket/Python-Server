0eae661e9fda09a798938a7b57626f8d6b34d339
sasha <sasha@sashas-MacBook-Air.local>
2018-04-24 19:57:13 -0700

    added commenting for better readability

--- server.py
+++ server.py
@@ -2,21 +2,30 @@ from bottle import route, run
 from pymongo import MongoClient
 import json
 
+# intializes the mongoDB connection
+
 client = MongoClient()
 db = client.documents
 
+# Adds a docs part
 docs = db.docs
 
+# Creates data for a new doc
 doc_data = {
 	'title': 'Random Placeholder Title',
 	'content': 'lorum ipsum something something something',
 	'owneruid': '1234abcd'
 }
+# Inserts doc
 results = docs.insert_one(doc_data)
 
+# gets requests to the server
 @route('/db/<uid>')
 def getDoc(uid):
+	# logos UID request
 	print(uid)
+	# returns applicable data to client
 	return "Title: " + docs.find_one({'owneruid': uid}).get('title') + " Content: " + docs.find_one({'owneruid': uid}).get('content')
 
+# runs server
 run(host='localhost', port=8080, debug=True)
